dfc75570fe7cd068f0df9e0f8c57c657f9ec79ed
Juan José Quiroz Omaña <juanjqogm@gmail.com>
2023-01-16 20:45:13 +0900

    Added support for semantic scholar academic API

--- my-website/src/pages/tests.js
+++ my-website/src/pages/tests.js
@@ -1,32 +1,36 @@
-
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 import Layout from '@theme/Layout';
+
 import styles from './index.module.css';
+export default function App() {
+  const [data, setData] = useState({});
 
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        "https://api.semanticscholar.org/graph/v1/paper/6fe5d5713b626139f880925188980ec95a82a631?fields=title,citationCount,citations.title,citations.authors,citations.year,citations.publicationTypes,citations.venue,citations.url,citations.openAccessPdf"
+      );
+      const parsed = await response.json();
+      setData(parsed);
+    })();
+  }, []);
 
-export default function MyReactPage() {
-  
   return (
- 
     <Layout>
       <div className="container">
+         <section className={styles.always_light}>   
+            <h1>Conference/Journal papers that used DQ Robotics</h1>
+              <iframe src="https://bibbase.org/show?bib=dqrobotics.github.io%2Fimages%2Fdqrobotics.bib" frameborder="0" allowfullscreen
+                 width={1000} height={500}>
 
-      <section className={styles.always_light}>   
-      <h1>Conference/Journal papers that used DQ Robotics</h1>
-      
-         <div >
-           <iframe src="https://github.com/dqrobotics/matlab/blob/master/CONTRIBUTING.md" frameborder="0" allowfullscreen
-             width={1000} height={500}>
-
-          </iframe>
-
-         
+              </iframe>
+        <div>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
         </div>
-      </section>
-
+        </section>
       </div>
-      </Layout>
-    
+    </Layout>
   );
 }
 
