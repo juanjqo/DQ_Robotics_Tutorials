bc01b4c2bba1e75b7c3445bd608c09cc73fac349
Juan José Quiroz Omaña <juanjqogm@gmail.com>
2023-01-18 18:07:43 +0900

    Added support for katex

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -40,6 +40,7 @@ const config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            path: 'docs',
             remarkPlugins: [math],
             rehypePlugins: [katex],
         },
@@ -56,7 +57,16 @@ const config = {
       }),
     ],
 
-
+  ],
+  
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
   ],
 
   themeConfig:
